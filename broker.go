f6bb5ba312e475cd0cc39646bae1f0db19a0d7cf
Travis Bischel <travis.bischel@gmail.com>
2019-03-29 00:26:42 -0400

    finalize broker look&feel

--- broker.go
+++ broker.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"math"
 	"net"
 	"sync"
 	"sync/atomic"
@@ -25,23 +26,37 @@ type promisedResp struct {
 
 type apiVersions [kmsg.MaxKey + 1]int16
 
+// broker manages the concept how a client would interact with a broker.
 type broker struct {
 	cl *Client
 
+	// id and addr are the Kafka broker ID and addr for this broker.
 	id   int32
 	addr string
 
-	cxnMu sync.Mutex
-	cxn   *brokerCxn
+	// cxn manages a single tcp connection to a broker.
+	// This field is managed serially in handleReqs.
+	cxn *brokerCxn
 
+	// bufferedReq manages buffering writes for a produce request.
 	bufferedReq bufferedProduceRequest
 
-	reqs chan promisedReq
-
+	// dieMu guards sending to reqs in case the broker has been
+	// permanently stopped.
 	dieMu sync.RWMutex
-	// dead is an atomic so that a fully backed up request chan
-	// does not block a shutdown.
+	// reqs manages incoming message requests.
+	reqs chan promisedReq
+	// dead is an atomic so a backed up reqs cannot block broker stoppage.
 	dead int64
+}
+
+const unknownControllerID = -1
+
+// broker IDs are all positive, but Kafka uses -1 to signify unknown
+// controllers. To avoid issues where a client broker ID map knows of
+// a -1 ID controller, we start unknown seeds at MinInt32.
+func unknownSeedID(seedNum int) int32 {
+	return int32(math.MinInt32 + seedNum)
 }
 
 func (c *Client) newBroker(addr string, id int32) *broker {
@@ -60,81 +75,7 @@ func (c *Client) newBroker(addr string, id int32) *broker {
 	return b
 }
 
-func (b *broker) loadConnection() (*brokerCxn, error) {
-	b.cxnMu.Lock()
-	cxn := b.cxn
-	b.cxnMu.Unlock()
-
-	if cxn != nil {
-		return cxn, nil
-	}
-
-	// we have no connection: connect on demand
-	conn, err := b.connect()
-	if err != nil {
-		return nil, err
-	}
-
-	cxn = &brokerCxn{
-		conn:     conn,
-		clientID: b.cl.cfg.client.id,
-	}
-	if err = cxn.init(); err != nil {
-		conn.Close()
-		return nil, err
-	}
-
-	b.cxn = cxn
-	return cxn, nil
-}
-
-// connect connects to the broker's addr, returning the new connection.
-//
-// Should we not wait for handshake? Should we not do any network IO?
-// Should we not wait for api versions response?
-func (b *broker) connect() (net.Conn, error) {
-	conn, err := b.cl.cfg.client.dialFn(b.addr)
-	if err != nil {
-		return nil, err
-	}
-	if b.cl.cfg.client.tlsCfg != nil {
-		tlsconn := tls.Client(conn, b.cl.cfg.client.tlsCfg)
-		// TODO SetDeadline, then clear
-		if err = tlsconn.Handshake(); err != nil {
-			conn.Close()
-			return nil, err
-		}
-		conn = tlsconn
-	}
-
-	return conn, nil
-}
-
-// readResponse reads a response from conn and ensures that the correlation
-// ID matches, returning a newly allocated slice on success or an error.
-func readResponse(conn io.Reader, correlationID int32) ([]byte, error) {
-	sizeBuf := make([]byte, 4)
-	if _, err := io.ReadFull(conn, sizeBuf[:4]); err != nil {
-		return nil, err
-	}
-	size := int32(binary.BigEndian.Uint32(sizeBuf[:4]))
-
-	buf := make([]byte, size)
-	if _, err := io.ReadFull(conn, buf); err != nil {
-		return nil, err
-	}
-
-	if len(buf) < 4 {
-		return nil, errNotEnoughData
-	}
-	gotID := int32(binary.BigEndian.Uint32(buf))
-	buf = buf[4:]
-	if gotID != correlationID {
-		return nil, errCorrelationIDMismatch
-	}
-	return buf, nil
-}
-
+// stopForever permanently disables this broker.
 func (b *broker) stopForever() {
 	if atomic.SwapInt64(&b.dead, 1) == 1 {
 		return
@@ -151,7 +92,7 @@ func (b *broker) stopForever() {
 	b.dieMu.Lock()
 	b.dieMu.Unlock()
 
-	// after mu, nothing will be sent down reqs
+	// after dieMu, nothing will be sent down reqs
 	close(b.reqs)
 }
 
@@ -209,14 +150,16 @@ func (b *broker) wait(
 	promise(resp, err)
 }
 
+// handleReqs manages the intake of message requests for a broker.
+//
+// This creates connections as appropriate, serializes the request, and sends
+// awaiting responses with the request promise to be handled as appropriate.
+//
+// If any of these steps fail, the promise is called with the relevant error.
 func (b *broker) handleReqs() {
 	defer func() {
-		b.cxnMu.Lock()
-		cxn := b.cxn
-		b.cxnMu.Unlock()
-
-		if cxn != nil {
-			cxn.die()
+		if b.cxn != nil {
+			b.cxn.die()
 		}
 	}()
 
@@ -227,7 +170,7 @@ func (b *broker) handleReqs() {
 			continue
 		}
 
-		// We have a cxn: version bound our message and write it.
+		// version bound our request
 		req := pr.req
 		brokerMax := cxn.versions[req.Key()]
 		ourMax := req.MaxVersion()
@@ -248,7 +191,7 @@ func (b *broker) handleReqs() {
 			continue
 		}
 
-		cxn.enqueueResp(promisedResp{
+		cxn.waitResp(promisedResp{
 			correlationID,
 			req.ResponseKind(),
 			pr.promise,
@@ -256,21 +199,71 @@ func (b *broker) handleReqs() {
 	}
 }
 
+// loadConection returns the broker's connection, creating it if necessary
+// and returning an error of if that fails.
+func (b *broker) loadConnection() (*brokerCxn, error) {
+	if b.cxn != nil {
+		return b.cxn, nil
+	}
+
+	conn, err := b.connect()
+	if err != nil {
+		return nil, err
+	}
+
+	cxn := &brokerCxn{
+		conn:     conn,
+		clientID: b.cl.cfg.client.id,
+	}
+	if err = cxn.init(); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	b.cxn = cxn
+	return cxn, nil
+}
+
+// connect connects to the broker's addr, returning the new connection.
+func (b *broker) connect() (net.Conn, error) {
+	conn, err := b.cl.cfg.client.dialFn(b.addr)
+	if err != nil {
+		return nil, err
+	}
+	if b.cl.cfg.client.tlsCfg != nil {
+		tlsconn := tls.Client(conn, b.cl.cfg.client.tlsCfg)
+		// TODO SetDeadline, then clear
+		if err = tlsconn.Handshake(); err != nil {
+			conn.Close()
+			return nil, err
+		}
+		conn = tlsconn
+	}
+
+	return conn, nil
+}
+
+// brokerCxn manages an actual connection to a Kafka broker. This is separate
+// the broker struct to allow lazy connection (re)creation.
 type brokerCxn struct {
-	conn          net.Conn
+	conn     net.Conn
+	versions apiVersions
+
+	// reqBuf, correlationID, and clientID are used in writing requests.
 	reqBuf        []byte
-	versions      apiVersions
 	correlationID int32
 	clientID      *string
 
-	mu    sync.RWMutex
+	// dieMu guards sending to resps in case the connection has died.
+	dieMu sync.RWMutex
+	// resps manages reading kafka responses.
 	resps chan promisedResp
-	// dead is an atomic so that a completely backed up
-	// resps chan cannot block connection killing.
+	// dead is an atomic so that a backed up resps cannot block cxn death.
 	dead int64
 }
 
 func (cx *brokerCxn) init() error {
+	// TODO sasl
 	if err := cx.requestAPIVersions(); err != nil {
 		return err
 	}
@@ -285,6 +278,7 @@ func (cx *brokerCxn) requestAPIVersions() (err error) {
 	if err != nil {
 		return err
 	}
+
 	rawResp, err := readResponse(cx.conn, corrID)
 	if err != nil {
 		return err
@@ -303,6 +297,7 @@ func (cx *brokerCxn) requestAPIVersions() (err error) {
 	return nil
 }
 
+// errWrite is the error returned when a message request write fails.
 type errWrite struct {
 	wrote int
 	err   error
@@ -312,6 +307,8 @@ func (e *errWrite) Error() string {
 	return fmt.Sprintf("err after %d bytes written: %v", e.wrote, e.err)
 }
 
+// writeRequest writes a message request to the broker connection, bumping the
+// connection's correlation ID as appropriate for the next write.
 func (cx *brokerCxn) writeRequest(req kmsg.Request) (int32, error) {
 	cx.reqBuf = kmsg.AppendRequest(
 		cx.reqBuf[:0],
@@ -328,12 +325,42 @@ func (cx *brokerCxn) writeRequest(req kmsg.Request) (int32, error) {
 	return id, nil
 }
 
+// readResponse reads a response from conn, ensures the correlation ID is
+// correct, and returns a newly allocated slice on success.
+func readResponse(conn io.Reader, correlationID int32) ([]byte, error) {
+	sizeBuf := make([]byte, 4)
+	if _, err := io.ReadFull(conn, sizeBuf[:4]); err != nil {
+		return nil, err
+	}
+	size := int32(binary.BigEndian.Uint32(sizeBuf[:4]))
+	if size < 0 {
+		return nil, errInvalidResp
+	}
+
+	buf := make([]byte, size)
+	if _, err := io.ReadFull(conn, buf); err != nil {
+		return nil, err
+	}
+
+	if len(buf) < 4 {
+		return nil, errNotEnoughData
+	}
+	gotID := int32(binary.BigEndian.Uint32(buf))
+	buf = buf[4:]
+	if gotID != correlationID {
+		return nil, errCorrelationIDMismatch
+	}
+	return buf, nil
+}
+
+// die kills a broker connection (which could be dead already) and replies to
+// all requests awaiting responses appropriately.
 func (cx *brokerCxn) die() {
 	if atomic.SwapInt64(&cx.dead, 1) == 1 {
 		return
 	}
 
-	cx.conn.Close() // interrupt any write
+	cx.conn.Close()
 
 	go func() {
 		for pr := range cx.resps {
@@ -341,38 +368,41 @@ func (cx *brokerCxn) die() {
 		}
 	}()
 
-	cx.mu.Lock()
-	cx.mu.Unlock()
+	cx.dieMu.Lock()
+	cx.dieMu.Unlock()
 
 	close(cx.resps) // after lock, nothing sends down resps
 }
 
-func (cx *brokerCxn) enqueueResp(pr promisedResp) {
+// waitResp, called serially by a broker's handleReqs, manages handling a
+// message requests's response.
+func (cx *brokerCxn) waitResp(pr promisedResp) {
 	dead := false
 
-	cx.mu.RLock()
+	cx.dieMu.RLock()
 	if atomic.LoadInt64(&cx.dead) == 1 {
 		dead = true
 	} else {
 		cx.resps <- pr
 	}
-	cx.mu.RUnlock()
+	cx.dieMu.RUnlock()
 
 	if dead {
 		pr.promise(nil, errBrokerConnectionDied)
 	}
 }
 
+// handleResps handles all broker responses serially.
 func (cx *brokerCxn) handleResps() {
-	defer cx.die()
+	defer cx.die() // always track our death
 
-	rawSize := make([]byte, 4)
 	for pr := range cx.resps {
 		raw, err := readResponse(cx.conn, pr.correlationID)
 		if err != nil {
 			pr.promise(nil, err)
 			return
 		}
+		fmt.Println("got resp\n", raw)
 		pr.promise(pr.resp, pr.resp.ReadFrom(raw))
 	}
 }
