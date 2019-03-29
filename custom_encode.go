043f3e98a02074ba5284dfbf15dd5e7ab35db4a0
Travis Bischel <travis.bischel@gmail.com>
2019-03-29 05:57:13 -0400

    custom_encode: fix kmsg => kbin moving

--- custom_encode.go
+++ custom_encode.go
@@ -3,6 +3,7 @@ package kgo
 import (
 	"hash/crc32"
 
+	"github.com/twmb/kgo/kbin"
 	"github.com/twmb/kgo/kmsg"
 )
 
@@ -27,16 +28,16 @@ func (m *messageBufferedProduceRequest) SetVersion(v int16) { m.version = v }
 func (m *messageBufferedProduceRequest) GetVersion() int16  { return m.version }
 func (m *messageBufferedProduceRequest) AppendTo(dst []byte) []byte {
 	if m.version >= 3 {
-		dst = kmsg.AppendNullableString(dst, nil) // TODO transactional ID
+		dst = kbin.AppendNullableString(dst, nil) // TODO transactional ID
 	}
-	dst = kmsg.AppendInt16(dst, m.acks)
-	dst = kmsg.AppendInt32(dst, m.timeout)
-	dst = kmsg.AppendArrayLen(dst, len(m.data))
+	dst = kbin.AppendInt16(dst, m.acks)
+	dst = kbin.AppendInt32(dst, m.timeout)
+	dst = kbin.AppendArrayLen(dst, len(m.data))
 	for topic, partitions := range m.data {
-		dst = kmsg.AppendString(dst, topic)
-		dst = kmsg.AppendArrayLen(dst, len(partitions))
+		dst = kbin.AppendString(dst, topic)
+		dst = kbin.AppendArrayLen(dst, len(partitions))
 		for partition, batch := range partitions {
-			dst = kmsg.AppendInt32(dst, partition)
+			dst = kbin.AppendInt32(dst, partition)
 			dst = batch.appendTo(dst)
 		}
 	}
@@ -48,52 +49,52 @@ func (m *messageBufferedProduceRequest) ResponseKind() kmsg.Response {
 }
 
 func (r *recordBatch) appendTo(dst []byte) []byte {
-	dst = kmsg.AppendInt32(dst, r.wireLength-4) // NULLABLE_BYTES leading length, minus itself
-	dst = kmsg.AppendInt64(dst, 0)              // firstOffset, defined as zero for producing
+	dst = kbin.AppendInt32(dst, r.wireLength-4) // NULLABLE_BYTES leading length, minus itself
+	dst = kbin.AppendInt64(dst, 0)              // firstOffset, defined as zero for producing
 
-	dst = kmsg.AppendInt32(dst, r.wireLength-4-8-4) // minus nullable bytes, minus baseOffset, minus self
+	dst = kbin.AppendInt32(dst, r.wireLength-4-8-4) // minus nullable bytes, minus baseOffset, minus self
 
-	dst = kmsg.AppendInt32(dst, -1) // partitionLeaderEpoch, unused in clients
-	dst = kmsg.AppendInt8(dst, 2)   // magic, defined as 2 for records v0.11.0.0+
+	dst = kbin.AppendInt32(dst, -1) // partitionLeaderEpoch, unused in clients
+	dst = kbin.AppendInt8(dst, 2)   // magic, defined as 2 for records v0.11.0.0+
 
 	crcStart := len(dst)           // fill at end
-	dst = kmsg.AppendInt32(dst, 0) // reserved crc
+	dst = kbin.AppendInt32(dst, 0) // reserved crc
 
-	dst = kmsg.AppendInt16(dst, r.attrs)
-	dst = kmsg.AppendInt32(dst, int32(len(r.records)-1)) // lastOffsetDelta
-	dst = kmsg.AppendInt64(dst, r.firstTimestamp)
+	dst = kbin.AppendInt16(dst, r.attrs)
+	dst = kbin.AppendInt32(dst, int32(len(r.records)-1)) // lastOffsetDelta
+	dst = kbin.AppendInt64(dst, r.firstTimestamp)
 
 	// maxTimestamp is the timestamp of the last record in a batch.
 	lastRecord := r.records[len(r.records)-1]
-	dst = kmsg.AppendInt64(dst, r.firstTimestamp+int64(lastRecord.n.timestampDelta))
+	dst = kbin.AppendInt64(dst, r.firstTimestamp+int64(lastRecord.n.timestampDelta))
 
-	dst = kmsg.AppendInt64(dst, -1) // producerId
-	dst = kmsg.AppendInt16(dst, -1) // producerEpoch
-	dst = kmsg.AppendInt32(dst, -1) // baseSequence
+	dst = kbin.AppendInt64(dst, -1) // producerId
+	dst = kbin.AppendInt16(dst, -1) // producerEpoch
+	dst = kbin.AppendInt32(dst, -1) // baseSequence
 
 	// TODO compression: grab len here, compress, overwrite if shorter
 	// and update attr
-	dst = kmsg.AppendArrayLen(dst, len(r.records))
+	dst = kbin.AppendArrayLen(dst, len(r.records))
 	for _, pnr := range r.records {
 		dst = pnr.appendTo(dst)
 	}
 
-	kmsg.AppendInt32(dst[:crcStart], int32(crc32.Checksum(dst[crcStart+4:], crc32c)))
+	kbin.AppendInt32(dst[:crcStart], int32(crc32.Checksum(dst[crcStart+4:], crc32c)))
 
 	return dst
 }
 
 func (pnr promisedNumberedRecord) appendTo(dst []byte) []byte {
-	dst = kmsg.AppendVarint(dst, pnr.n.lengthField)
-	dst = kmsg.AppendInt8(dst, 0) // attributes, currently unused
-	dst = kmsg.AppendVarint(dst, pnr.n.timestampDelta)
-	dst = kmsg.AppendVarint(dst, pnr.n.offsetDelta)
-	dst = kmsg.AppendVarintBytes(dst, pnr.pr.r.Key)
-	dst = kmsg.AppendVarintBytes(dst, pnr.pr.r.Value)
-	dst = kmsg.AppendVarint(dst, int32(len(pnr.pr.r.Headers)))
+	dst = kbin.AppendVarint(dst, pnr.n.lengthField)
+	dst = kbin.AppendInt8(dst, 0) // attributes, currently unused
+	dst = kbin.AppendVarint(dst, pnr.n.timestampDelta)
+	dst = kbin.AppendVarint(dst, pnr.n.offsetDelta)
+	dst = kbin.AppendVarintBytes(dst, pnr.pr.r.Key)
+	dst = kbin.AppendVarintBytes(dst, pnr.pr.r.Value)
+	dst = kbin.AppendVarint(dst, int32(len(pnr.pr.r.Headers)))
 	for _, h := range pnr.pr.r.Headers {
-		dst = kmsg.AppendVarintString(dst, h.Key)
-		dst = kmsg.AppendVarintBytes(dst, h.Value)
+		dst = kbin.AppendVarintString(dst, h.Key)
+		dst = kbin.AppendVarintBytes(dst, h.Value)
 	}
 	return dst
 }
