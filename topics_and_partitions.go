6d9809b9a1135216e470f3ce9959b6abfa9bba4d
Travis Bischel <twmb@protonmail.com>
2019-08-10 04:29:29 -0400

    topics and partitions: docs, move funcs around, add one new func
    New func returns topics and their partition numbers, not anything more.
    For use in anything that only needs map[string][]int32.

--- topics_and_partitions.go
+++ topics_and_partitions.go
@@ -5,21 +5,67 @@ import (
 	"sync/atomic"
 )
 
+// loadTopics returns the client's current topics and their partitions.
 func (c *Client) loadTopics() map[string]*topicPartitions {
 	return c.topics.Load().(map[string]*topicPartitions)
 }
 
+// cloneTopics returns a copy of the client's current topics and partitions.
+// This is a shallow copy; only the map is copied.
 func (c *Client) cloneTopics() map[string]*topicPartitions {
 	old := c.loadTopics()
 	new := make(map[string]*topicPartitions, len(old)+5)
-	for topic, topicPartition := range old {
-		new[topic] = topicPartition
+	for topic, partitions := range old {
+		new[topic] = partitions
 	}
 	return new
 }
 
+// loadShortTopics returns topic names and a copy of their partition numbers.
+func (c *Client) loadShortTopics() map[string][]int32 {
+	topics := c.loadTopics()
+	short := make(map[string][]int32, len(topics))
+	for topic, partitions := range topics {
+		short[topic] = append([]int32(nil), partitions.load().partitions...)
+	}
+	return short
+}
+
+func newTopicPartitions() *topicPartitions {
+	parts := new(topicPartitions)
+	parts.v.Store(&topicPartitionsData{
+		all:      make(map[int32]*topicPartition),
+		writable: make(map[int32]*topicPartition),
+	})
+	parts.c = sync.NewCond(parts.mu.RLocker())
+	return parts
+}
+
+// topicPartitions contains all information about a topic's partitions.
+type topicPartitions struct {
+	v atomic.Value // *topicPartitionsData
+
+	mu sync.RWMutex
+	c  *sync.Cond
+}
+
+func (t *topicPartitions) load() *topicPartitionsData {
+	return t.v.Load().(*topicPartitionsData)
+}
+
+// topicPartitionsData is the data behind a topicPartitions' v.
+//
+// We keep this in an atomic because it is expected to be extremely read heavy,
+// and if it were behind a lock, the lock would need to be held for a while.
+type topicPartitionsData struct {
+	loadErr    error // could be auth, unknown, leader not avail, or creation err
+	partitions []int32
+	all        map[int32]*topicPartition // partition num => partition
+	writable   map[int32]*topicPartition // partition num => partition, eliding partitions with no leader / listener
+}
+
 // topicPartition contains all information from Kafka for a topic's partition,
-// as well as all records waiting to be sent to it.
+// as well as what a client is producing to it or info about consuming from it.
 type topicPartition struct {
 	topic     string
 	partition int32
@@ -36,19 +82,18 @@ type topicPartition struct {
 	consumption *consumption
 }
 
+// migrateProductionTo is called on metadata update if a topic partition's sink
+// has changed. This moves record production from one sink to the other; this
+// must be done such that records produced during migration follow those
+// already buffered.
 func (old *topicPartition) migrateProductionTo(new *topicPartition) {
-	// If the new sink is different, we have to migrate any
-	// buffered records safely and we must do it such that new
-	// records being produced during this migration will still be
-	// sent after buffered records, not before.
-
-	// First, we remove the records from the old source.
-	old.records.sink.removeSource(old.records)
+	old.records.sink.removeSource(old.records) // first, remove our record source from the old sink
 
 	// Before this next lock, record producing will buffer to the
 	// in-migration-progress records and may trigger draining to
 	// the old sink. That is fine, the old sink no longer consumes
 	// from these records. We just have wasted drain triggers.
+
 	old.records.mu.Lock() // guard setting sink
 	old.records.sink = new.records.sink
 	old.records.mu.Unlock()
@@ -57,57 +102,24 @@ func (old *topicPartition) migrateProductionTo(new *topicPartition) {
 	// on the new sink, which is not yet consuming from these
 	// records. Again, just more wasted drain triggers.
 
-	// Once we add this source to the new sink (through the old
-	// topic partitions pointers), the new sink will begin
-	// draining our records.
-	old.records.sink.addSource(old.records)
+	old.records.sink.addSource(old.records) // add our record source to the new sink
 
-	// Finally, copy the records pointer to our new topic
-	// partitions, which will be atomically stored on defer.
+	// At this point, the new sink will be draining our records. We lastly
+	// need to copy the records pointer to our new topicPartition.
 	new.records = old.records
 }
 
+// migrateConsumptionTo is called on metadata update if a topic partition's
+// source has changed. This happens whenever the sink changes, since a source
+// is just the counterpart to a sink for the same topic partition.
+//
+// The pattern is the same as above, albeit no concurrent-produce issues to
+// worry about.
 func (old *topicPartition) migrateConsumptionTo(new *topicPartition) {
-	// This follows much the same pattern as before, but we have fewer
-	// subtle issues around needing to be careful here.
 	old.consumption.source.removeConsumption(old.consumption)
 	old.consumption.mu.Lock()
 	old.consumption.source = new.consumption.source
 	old.consumption.mu.Unlock()
 	old.consumption.source.addConsumption(old.consumption)
-}
-
-// topicPartitionsData is the data behind a topicPartitions' v.
-//
-// We keep this in an atomic because it is expected to be extremely read heavy,
-// and if it were behind a lock, the lock would need holding for a good chunk
-// of code.
-type topicPartitionsData struct {
-	loadErr    error // could be auth, unknown, leader not avail, or creation err
-	partitions []int32
-	all        map[int32]*topicPartition // partition num => partition
-	writable   map[int32]*topicPartition // partition num => partition, eliding partitions with no leader / listener
-}
-
-// topicPartitions contains all information about a topic's partitions.
-type topicPartitions struct {
-	v atomic.Value // *topicPartitionsData
-
-	mu sync.RWMutex
-	c  *sync.Cond
-}
-
-func (t *topicPartitions) load() *topicPartitionsData {
-	return t.v.Load().(*topicPartitionsData)
-}
-
-// newTopicParts creates and returns new topicPartitions
-func newTopicParts() *topicPartitions {
-	parts := new(topicPartitions)
-	parts.v.Store(&topicPartitionsData{
-		all:      make(map[int32]*topicPartition),
-		writable: make(map[int32]*topicPartition),
-	})
-	parts.c = sync.NewCond(parts.mu.RLocker())
-	return parts
+	new.consumption = old.consumption
 }
