twmb/franz-go

commit a3d9f7363e45ea15423dd11fba7ca400bfea1dc7

sink: general improvements
This reorganizes some code to hopefully be a bit easier to understand.

* creates removeFromTxn, replaces the duplicate areas where a partition
is removed from a transaction

* removes requeueUnattemptedReq in favor of just using
handleRetryBatches. Unattempted requests may as well just be handled
similarly -- the only downside is a small backoff, which is likely
necessary anyway because unattempted requests are usually connection
failures, and we do not want to stampede reconnect.

* extracts the noack handling to a separate function. This makes reading
handleReqResp a bit simpler.

* extracts hook metrics to a separate type / function, which again makes
handleReqResp a bit simpler.

* removes tryResetFailingBatchesWith, moves the logic inline into
handleRetryBatches. tryResetFailingBatchesWith was used twice, once by
the now removed requeueUnattemptedReq. It is simpler to reason about
inline, now.

* panics if our inflight on the recBuf goes negative. This is just a
sanity check; it never should.

pkg/kgo/sink.go
@@ -405,16 +405,18 @@ func (s *sink) doTxnReq(
 	// not be trying to add them to a partition if they were not.
 	defer func() {
 		if err != nil {
-			req.batches.eachOwnerLocked(func(batch seqRecBatch) {
-				batch.owner.addedToTxn = false
-				batch.owner.resetBatchDrainIdx()
-				batch.decInflight()
-			})
+			req.batches.eachOwnerLocked(seqRecBatch.removeFromTxn)
 		}
 	}()
 	return s.cl.doWithConcurrentTransactions("AddPartitionsToTxn", func() error {
 		return s.issueTxnReq(req, txnReq)
 	})
+}
+
+func (b seqRecBatch) removeFromTxn() {
+	b.owner.addedToTxn = false
+	b.owner.resetBatchDrainIdx()
+	b.decInflight()
 }
 
 func (s *sink) issueTxnReq(
@@ -450,9 +452,7 @@ func (s *sink) issueTxnReq(
 				// We are stripping this retriable-err batch from the request,
 				// so we must reset that it has been added to the txn.
 				batch.owner.mu.Lock()
-				batch.owner.addedToTxn = false
-				batch.owner.resetBatchDrainIdx()
-				batch.decInflight()
+				batch.removeFromTxn()
 				batch.owner.mu.Unlock()
 
 				delete(topicBatches, partition.Partition)
@@ -463,18 +463,6 @@ func (s *sink) issueTxnReq(
 		}
 	}
 	return nil
-}
-
-// Resets the drain indices for any first-batch.
-func (s *sink) requeueUnattemptedReq(req *produceRequest, err error) {
-	var maybeDrain bool
-	req.batches.tryResetFailingBatchesWith(&s.cl.cfg, false, func(seqRecBatch) {
-		maybeDrain = true
-	})
-	if maybeDrain {
-		s.maybeTriggerBackoff(req.backoffSeq)
-		s.maybeDrain()
-	}
 }
 
 // firstRespCheck is effectively a sink.Once. On the first response, if the
@@ -504,21 +492,52 @@ func (s *sink) firstRespCheck(idempotent bool, version int16) {
 // produce response.
 func (s *sink) handleReqClientErr(req *produceRequest, err error) {
 	switch {
-	case err == errChosenBrokerDead,
-		err == errUnknownBroker:
-		// A dead / unknown broker means the broker may have migrated,
-		// so we retry to force a metadata reload.
-		s.handleRetryBatches(req.batches, req.backoffSeq, false, false)
-
-	case err == ErrClientClosed:
-		s.cl.failBufferedRecords(ErrClientClosed)
-
 	default:
 		s.cl.cfg.logger.Log(LogLevelWarn, "random error while producing, requeueing unattempted request", "broker", logID(s.nodeID), "err", err)
 		fallthrough
 
-	case isRetriableBrokerErr(err):
-		s.requeueUnattemptedReq(req, err)
+	case err == errChosenBrokerDead,
+		err == errUnknownBroker,
+		isRetriableBrokerErr(err):
+		// A dead / unknown broker means the broker may have migrated,
+		// so we retry to force a metadata reload.
+		updateMeta := err == errUnknownBroker
+		s.handleRetryBatches(req.batches, req.backoffSeq, updateMeta, false)
+
+	case err == ErrClientClosed:
+		s.cl.failBufferedRecords(ErrClientClosed)
+	}
+}
+
+// No acks mean no response. The following block is basically an extremely
+// condensed version of the logic in handleReqResp.
+func (s *sink) handleReqRespNoack(b *bytes.Buffer, debug bool, req *produceRequest) {
+	if debug {
+		fmt.Fprintf(b, "noack ")
+	}
+	for topic, partitions := range req.batches {
+		if debug {
+			fmt.Fprintf(b, "%s[", topic)
+		}
+		for partition, batch := range partitions {
+			batch.owner.mu.Lock()
+			if batch.isOwnersFirstBatch() {
+				if debug {
+					fmt.Fprintf(b, "%d{0=>%d}, ", partition, len(batch.records))
+				}
+				s.cl.finishBatch(batch.recBatch, req.producerID, req.producerEpoch, partition, 0, nil)
+			} else if debug {
+				fmt.Fprintf(b, "%d{skipped}, ", partition)
+			}
+			batch.decInflight()
+			batch.owner.mu.Unlock()
+		}
+		if debug {
+			if bytes.HasSuffix(b.Bytes(), []byte(", ")) {
+				b.Truncate(b.Len() - 2)
+			}
+			b.WriteString("], ")
+		}
 	}
 }
 
@@ -529,10 +548,10 @@ func (s *sink) handleReqResp(br *broker, req *produceRequest, resp kmsg.Response
 	}
 	s.firstRespCheck(req.idempotent(), req.version)
 	atomic.StoreUint32(&s.consecutiveFailures, 0)
+	defer req.metrics.hook(&s.cl.cfg, br) // defer to end so that non-written batches are removed
 
 	var b *bytes.Buffer
 	debug := s.cl.cfg.logger.Level() >= LogLevelDebug
-
 	if debug {
 		b = bytes.NewBuffer(make([]byte, 0, 128))
 		defer func() {
@@ -542,57 +561,8 @@ func (s *sink) handleReqResp(br *broker, req *produceRequest, resp kmsg.Response
 		}()
 	}
 
-	// We defer updating the produce batch metrics in a goroutine; anything
-	// that we count as not-written (not the first batch, error) is removed
-	// from metrics before we return.
-	defer func() {
-		if len(req.metrics) > 0 {
-			s.cl.cfg.hooks.each(func(h Hook) {
-				if h, ok := h.(HookProduceBatchWritten); ok {
-					go func() {
-						for topic, partitions := range req.metrics {
-							for partition, metrics := range partitions { // I heard you liked tabs?
-								h.OnProduceBatchWritten(br.meta, topic, partition, metrics)
-							}
-						}
-					}()
-				}
-			})
-		}
-	}()
-
-	// If we have no acks, we will have no response. The following block is
-	// basically an extremely condensed version of everything that follows.
-	// We *do* retry on error even with no acks, because an error would
-	// mean the write itself failed.
 	if req.acks == 0 {
-		if debug {
-			fmt.Fprintf(b, "noack ")
-		}
-		for topic, partitions := range req.batches {
-			if debug {
-				fmt.Fprintf(b, "%s[", topic)
-			}
-			for partition, batch := range partitions {
-				batch.owner.mu.Lock()
-				if batch.isOwnersFirstBatch() {
-					if debug {
-						fmt.Fprintf(b, "%d{0=>%d}, ", partition, len(batch.records))
-					}
-					s.cl.finishBatch(batch.recBatch, req.producerID, req.producerEpoch, partition, 0, nil)
-				} else if debug {
-					fmt.Fprintf(b, "%d{skipped}, ", partition)
-				}
-				batch.decInflight()
-				batch.owner.mu.Unlock()
-			}
-			if debug {
-				if bytes.HasSuffix(b.Bytes(), []byte(", ")) {
-					b.Truncate(b.Len() - 2)
-				}
-				b.WriteString("], ")
-			}
-		}
+		s.handleReqRespNoack(b, debug, req)
 		return
 	}
 
@@ -890,6 +860,9 @@ func (cl *Client) finishBatch(batch *recBatch, producerID int64, producerEpoch i
 
 // handleRetryBatches sets any first-buf-batch to failing and triggers a
 // metadata that will eventually clear the failing state and re-drain.
+//
+// If idempotency is disabled, if a batch is timed out or hit the retry limit,
+// we fail it and anything after it.
 func (s *sink) handleRetryBatches(
 	retry seqRecBatches,
 	backoffSeq uint32,
@@ -897,7 +870,22 @@ func (s *sink) handleRetryBatches(
 	canFail bool, // if records can fail if they are at limits
 ) {
 	var needsMetaUpdate bool
-	retry.tryResetFailingBatchesWith(&s.cl.cfg, canFail, func(batch seqRecBatch) {
+	retry.eachOwnerLocked(func(batch seqRecBatch) {
+		defer batch.decInflight()
+
+		if !batch.isOwnersFirstBatch() {
+			return
+		}
+
+		if canFail || s.cl.cfg.disableIdempotency {
+			if err := batch.maybeFailErr(&s.cl.cfg); err != nil {
+				batch.owner.failAllRecords(err)
+				return
+			}
+		}
+
+		batch.owner.resetBatchDrainIdx()
+
 		if updateMeta {
 			batch.owner.failing = true
 			needsMetaUpdate = true
@@ -1376,8 +1364,12 @@ func (b *recBatch) decInflight() {
 	if recBuf.inflight != 0 {
 		return
 	}
+	if recBuf.inflight < 0 {
+		panic("record buffer went negative inflight!")
+	}
+	oldSink := recBuf.inflightOnSink
 	recBuf.inflightOnSink = nil
-	if recBuf.batchDrainIdx != len(recBuf.batches) {
+	if oldSink != recBuf.sink && recBuf.batchDrainIdx != len(recBuf.batches) {
 		recBuf.sink.maybeDrain()
 	}
 }
@@ -1407,7 +1399,7 @@ type produceRequest struct {
 	// sizes (in byteS) of each batch.
 	//
 	// We use this in handleReqResp for the OnProduceHook.
-	metrics map[string]map[int32]ProduceBatchMetrics
+	metrics produceMetrics
 
 	compressor *compressor
 
@@ -1417,6 +1409,32 @@ type produceRequest struct {
 	// we use the proper flexible numbers when calculating.
 	wireLength      int32
 	wireLengthLimit int32
+}
+
+type produceMetrics map[string]map[int32]ProduceBatchMetrics
+
+func (p produceMetrics) hook(cfg *cfg, br *broker) {
+	if len(p) == 0 {
+		return
+	}
+	var hooks []HookProduceBatchWritten
+	cfg.hooks.each(func(h Hook) {
+		if h, ok := h.(HookProduceBatchWritten); ok {
+			hooks = append(hooks, h)
+		}
+	})
+	if len(hooks) == 0 {
+		return
+	}
+	go func() {
+		for _, h := range hooks {
+			for topic, partitions := range p {
+				for partition, metrics := range partitions {
+					h.OnProduceBatchWritten(br.meta, topic, partition, metrics)
+				}
+			}
+		}
+	}()
 }
 
 func (r *produceRequest) idempotent() bool { return r.producerID >= 0 }
@@ -1504,31 +1522,6 @@ func (rbs *seqRecBatches) addSeqBatch(topic string, part int32, batch seqRecBatc
 		(*rbs)[topic] = topicBatches
 	}
 	topicBatches[part] = batch
-}
-
-// Resets the drain index for any batch that is the first in its record buffer,
-// as well as decrements the inflight for all batches always.
-//
-// If idempotency is disabled, if a batch is timed out or hit the retry limit,
-// we fail it and anything after it.
-func (rbs seqRecBatches) tryResetFailingBatchesWith(cfg *cfg, canFail bool, fn func(seqRecBatch)) {
-	rbs.eachOwnerLocked(func(batch seqRecBatch) {
-		defer batch.decInflight()
-
-		if !batch.isOwnersFirstBatch() {
-			return
-		}
-
-		if canFail || cfg.disableIdempotency {
-			if err := batch.maybeFailErr(cfg); err != nil {
-				batch.owner.failAllRecords(err)
-				return
-			}
-		}
-
-		batch.owner.resetBatchDrainIdx()
-		fn(batch)
-	})
 }
 
 func (rbs seqRecBatches) each(fn func(seqRecBatch)) {

commit 029f5e38eb16d18697729e85560524691dda1252

sink: fix decInflight double decrement bug
a3d9f7363e45ea15423dd11fba7ca400bfea1dc7 accidentally introduced a
double decrement: we would handle a batch (first decrement), and if we
needed to retry the batch, we would add it to be retried and decrement
again. This would result in a negative inflight, and when the partition
migrated to a different sink, it would never start draining because the
inflight count would never hit 0 and thus it would always be "inflight"
on the old sink. The panic that I added to catch a negative inflight was
three lines too low; the conditional just before the panic would return
on non-zero.

We now move the panic up to ensure it will be hit just in case, but more
importantly we simplify the inflight decrementing. This commit adds a
new batchesFlat field to the produceRequest. batchesFlat is not modified
after being issued, so now we can just range over all the batches that
were in the request when handling the response and decrement them all
inflight in one spot.

This replaces three places we were decrementing inflight with one.

pkg/kgo/sink.go
@@ -413,6 +413,9 @@ func (s *sink) doTxnReq(
 	})
 }
 
+// Removing a batch from the transaction means we will not be issuing it
+// inflight, and that it was not added to the txn and that we need to reset the
+// drain index.
 func (b seqRecBatch) removeFromTxn() {
 	b.owner.addedToTxn = false
 	b.owner.resetBatchDrainIdx()
@@ -529,7 +532,6 @@ func (s *sink) handleReqRespNoack(b *bytes.Buffer, debug bool, req *produceReque
 			} else if debug {
 				fmt.Fprintf(b, "%d{skipped}, ", partition)
 			}
-			batch.decInflight()
 			batch.owner.mu.Unlock()
 		}
 		if debug {
@@ -542,6 +544,11 @@ func (s *sink) handleReqRespNoack(b *bytes.Buffer, debug bool, req *produceReque
 }
 
 func (s *sink) handleReqResp(br *broker, req *produceRequest, resp kmsg.Response, err error) {
+	// The last thing we do before returning is to decrement the inflight
+	// count on all used batches. This func uses batchesFlat, meaning map
+	// deletions below do not affect us.
+	defer req.decBatchesInflight()
+
 	if err != nil {
 		s.handleReqClientErr(req, err)
 		return
@@ -644,7 +651,6 @@ func (s *sink) handleReqRespBatch(
 ) (retry, didProduce bool) {
 	batch.owner.mu.Lock()
 	defer batch.owner.mu.Unlock()
-	defer batch.decInflight()
 
 	nrec := len(batch.records)
 
@@ -873,8 +879,6 @@ func (s *sink) handleRetryBatches(
 ) {
 	var needsMetaUpdate bool
 	retry.eachOwnerLocked(func(batch seqRecBatch) {
-		defer batch.decInflight()
-
 		if !batch.isOwnersFirstBatch() {
 			return
 		}
@@ -1363,17 +1367,17 @@ func (b *recBatch) isTimedOut(limit time.Duration) bool {
 //
 // This is always called in the produce request path, not anywhere else (i.e.
 // not failAllRecords). We want inflight decrementing to be the last thing that
-// happens always for every request It does not matter if the records were
+// happens always for every request. It does not matter if the records were
 // independently failed: from the request issuing perspective, the batch is
 // still inflight.
 func (b *recBatch) decInflight() {
 	recBuf := b.owner
 	recBuf.inflight--
-	if recBuf.inflight != 0 {
-		return
-	}
 	if recBuf.inflight < 0 {
 		panic("record buffer went negative inflight!")
+	}
+	if recBuf.inflight != 0 {
+		return
 	}
 	oldSink := recBuf.inflightOnSink
 	recBuf.inflightOnSink = nil
@@ -1395,10 +1399,11 @@ type produceRequest struct {
 
 	backoffSeq uint32
 
-	txnID   *string
-	acks    int16
-	timeout int32
-	batches seqRecBatches
+	txnID       *string
+	acks        int16
+	timeout     int32
+	batches     seqRecBatches
+	batchesFlat []*recBatch
 
 	producerID    int64
 	producerEpoch int16
@@ -1497,7 +1502,17 @@ func (r *produceRequest) tryAddBatch(produceVersion int32, recBuf *recBuf, batch
 		recBuf.seq,
 		batch,
 	)
+	r.batchesFlat = append(r.batchesFlat, batch)
 	return true
+}
+
+// decBatchesInflight is called at the end of handling a req response.
+func (r *produceRequest) decBatchesInflight() {
+	for _, batch := range r.batchesFlat {
+		batch.owner.mu.Lock()
+		batch.decInflight()
+		batch.owner.mu.Unlock()
+	}
 }
 
 // seqRecBatch: a recBatch with a sequence number.