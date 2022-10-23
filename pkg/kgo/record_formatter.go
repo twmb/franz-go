47eccba6910fadb9abe08a84c21ac47899a9bae4
Travis Bischel <travis.bischel+github@gmail.com>
2022-10-23 13:45:17 -0600

    RecordFormatter: correct some documentation

--- pkg/kgo/record_formatter.go
+++ pkg/kgo/record_formatter.go
@@ -187,14 +187,14 @@ func (f *RecordFormatter) AppendPartitionRecord(b []byte, p *FetchPartition, r *
 //	%a{transactional-bit}
 //	%a{transactional-bit;bool}
 //
-// Prints 1 if the record is a transaction marker and 0 if the record is not a
-// transaction marker. Number formatting can be controlled with ";number".
+// Prints 1 if the record is a part of a transaction or 0 if it is not. Number
+// formatting can be controlled with ";number".
 //
 //	%a{control-bit}
 //	%a{control-bit;bool}
 //
-// Prints 1 if the record is a control record and 0 if the record is not a
-// control record. Number formatting can be controlled with ";number".
+// Prints 1 if the record is a commit marker or 0 if it is not. Number
+// formatting can be controlled with ";number".
 //
 // # Text
 //
