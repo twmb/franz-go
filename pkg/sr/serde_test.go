df66c94a2c439ddc4235bee50b3066d1de7de10e
Lovro Mažgon <lovro.mazgon@gmail.com>
2023-07-10 05:11:47 +0200

    Serde Header (#467)
    Adds the ability to customize the Serde encode & decode header.
    The internal default is the Confluent header.
    The next PRs will redo the serde opts and then add the function that actually allows setting a new header.

--- pkg/sr/serde_test.go
+++ pkg/sr/serde_test.go
@@ -135,7 +135,86 @@ func TestSerde(t *testing.T) {
 	if _, err := serde.DecodeNew([]byte{0, 0, 0, 0, 3}); err != io.EOF {
 		t.Errorf("got %v != exp io.EOF", err)
 	}
+	if _, err := serde.DecodeNew([]byte{0, 0, 0, 0, 3, 8}); err != ErrNotRegistered {
+		t.Errorf("got %v != exp ErrNotRegistered", err)
+	}
 	if _, err := serde.DecodeNew([]byte{0, 0, 0, 0, 99}); err != ErrNotRegistered {
 		t.Errorf("got %v != exp ErrNotRegistered", err)
+	}
+}
+
+func TestConfluentHeader(t *testing.T) {
+	var h confluentHeader
+
+	for i, test := range []struct {
+		id     int
+		index  []int
+		expEnc []byte
+	}{
+		{id: 1, index: nil, expEnc: []byte{0, 0, 0, 0, 1}},
+		{id: 256, index: nil, expEnc: []byte{0, 0, 0, 1, 0}},
+		{id: 2, index: []int{0}, expEnc: []byte{0, 0, 0, 0, 2, 0}},
+		{id: 3, index: []int{1}, expEnc: []byte{0, 0, 0, 0, 3, 2, 2}},
+		{id: 4, index: []int{1, 2, 3}, expEnc: []byte{0, 0, 0, 0, 4, 6, 2, 4, 6}},
+	} {
+		b, err := h.AppendEncode(nil, test.id, test.index)
+		if err != nil {
+			t.Errorf("#%d AppendEncode: got unexpected err %v", i, err)
+			continue
+		}
+		if !bytes.Equal(b, test.expEnc) {
+			t.Errorf("#%d: AppendEncode(%v) != exp(%v)", i, b, test.expEnc)
+			continue
+		}
+
+		if b2, _ := h.AppendEncode([]byte("foo"), test.id, test.index); !bytes.Equal(b2, append([]byte("foo"), b...)) {
+			t.Errorf("#%d got AppendEncode(%v) != AppendEncode(foo%v)", i, b2, b)
+		}
+
+		id, b2, err := h.DecodeID(b)
+		if err != nil {
+			t.Errorf("#%d DecodeID: got unexpected err %v", i, err)
+			continue
+		}
+		if id != test.id {
+			t.Errorf("#%d: DecodeID: id(%v) != exp(%v)", i, id, test.id)
+			continue
+		}
+		if test.index == nil && len(b2) != 0 {
+			t.Errorf("#%d: DecodeID: bytes(%v) != exp([])", i, b2)
+			continue
+		}
+
+		if test.index != nil {
+			index, b3, err := h.DecodeIndex(b2, len(test.index))
+			if err != nil {
+				t.Errorf("#%d DecodeIndex: got unexpected err %v", i, err)
+				continue
+			}
+			if !reflect.DeepEqual(index, test.index) {
+				t.Errorf("#%d: DecodeIndex: index(%v) != exp(%v)", i, index, test.index)
+				continue
+			}
+			if len(b3) != 0 {
+				t.Errorf("#%d: DecodeIndex: bytes(%v) != exp([])", i, b3)
+				continue
+			}
+		}
+	}
+
+	if _, _, err := h.DecodeID([]byte{1, 0, 0, 0, 0, 1}); err != ErrBadHeader {
+		t.Errorf("got %v != exp ErrBadHeader", err)
+	}
+	if _, _, err := h.DecodeID([]byte{0, 0, 0, 0}); err != ErrBadHeader {
+		t.Errorf("got %v != exp ErrBadHeader", err)
+	}
+	if _, _, err := h.DecodeIndex([]byte{2}, 1); err != io.EOF {
+		t.Errorf("got %v != exp io.EOF", err)
+	}
+	if _, _, err := h.DecodeIndex([]byte{6, 2, 4, 6}, 2); err != ErrNotRegistered {
+		t.Errorf("got %v != exp ErrNotRegistered", err)
+	}
+	if _, _, err := h.DecodeIndex([]byte{1}, 2); err != ErrBadHeader {
+		t.Errorf("got %v != exp ErrBadHeader", err)
 	}
 }
