474557f754df27abe0d417fdfb3b4857d135ded8
Travis Bischel <travis.bischel+github@gmail.com>
2025-03-10 15:36:51 -0600

    kgo: attempt to work around a Kafka hanging transaction bug
    What this adds:
    * After EndTxn, we send ListTransactions to verify the transaction has
      actually completed
    * If the transaction hangs for 10s, we log and set internally to
      reinitialize the producer ID.
    * We log a lot
    
    Some logging changes will be kept; the ListTransactions verification
    will be deleted, as well as the hard-resetting of the producerID.

--- pkg/kgo/group_test.go
+++ pkg/kgo/group_test.go
@@ -25,7 +25,7 @@ func TestGroupETL(t *testing.T) {
 	t.Parallel()
 
 	topic1, topic1Cleanup := tmpTopic(t)
-	defer topic1Cleanup()
+	t.Cleanup(topic1Cleanup)
 
 	errs := make(chan error)
 	body := []byte(randsha()) // a small body so we do not flood RAM
