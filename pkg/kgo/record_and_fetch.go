3ba6992d847b712b367b32ddfd410eb465af3737
Vladimir Varankin <vladimir@varank.in>
2025-09-04 23:39:28 +0200

    expand code comments

--- pkg/kgo/record_and_fetch.go
+++ pkg/kgo/record_and_fetch.go
@@ -465,6 +465,8 @@ func (fs Fetches) EachError(fn func(string, int32, error)) {
 // RecordIter returns an iterator over all records in a fetch.
 //
 // Note that errors should be inspected as well.
+//
+// Alternatively, use [RecordsAll] for a native Go iterator over records in the fetch.
 func (fs Fetches) RecordIter() *FetchesRecordIter {
 	iter := &FetchesRecordIter{fetches: fs}
 	iter.prepareNext()
