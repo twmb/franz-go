fb35983906c5049f7d4e779e3e152a9c3d425f35
Riadh <22998716+KiKoS0@users.noreply.github.com>
2025-12-15 18:23:23 +0100

    bring back conditional of failing when receiving records unexpectedly

--- pkg/kgo/group_test.go
+++ pkg/kgo/group_test.go
@@ -192,6 +192,9 @@ func (c *testConsumer) etl(etlsBeforeQuit int) {
 				}
 				if fetches.NumRecords() == 0 {
 					continue
+				} else {
+					c.errCh <- fmt.Errorf("poll got err %s but also unexpectedly received %d records", err, fetches.NumRecords())
+					return
 				}
 			}
 			if fetchErrs := fetches.Errors(); len(fetchErrs) > 0 {
