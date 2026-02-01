c3b185fb0a5fb336e2c08406540cb431cf3a8c06
Travis Bischel <travis.bischel+github@gmail.com>
2026-02-01 14:49:10 -0700

    kfake: add log level flag to server

--- pkg/kfake/main.go
+++ pkg/kfake/main.go
@@ -3,17 +3,40 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/twmb/franz-go/pkg/kfake"
 )
 
 func main() {
+	// Configure log level from flag
+	var logLevelStr string
+	flag.StringVar(&logLevelStr, "log-level", "none", "Log level: none, error, warn, info, debug")
+	flag.StringVar(&logLevelStr, "l", "none", "Log level (shorthand)")
+	flag.Parse()
+
+	logLevel := kfake.LogLevelNone
+	switch strings.ToLower(logLevelStr) {
+	case "debug":
+		logLevel = kfake.LogLevelDebug
+	case "info":
+		logLevel = kfake.LogLevelInfo
+	case "warn":
+		logLevel = kfake.LogLevelWarn
+	case "error":
+		logLevel = kfake.LogLevelError
+	case "none":
+		logLevel = kfake.LogLevelNone
+	}
+
 	c, err := kfake.NewCluster(
 		kfake.Ports(9092, 9093, 9094),
 		kfake.SeedTopics(-1, "foo"),
+		kfake.WithLogger(kfake.BasicLogger(os.Stderr, logLevel)),
 	)
 	if err != nil {
 		panic(err)
