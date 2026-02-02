8e86dee247227938110c56b2b30d95b249412d53
Travis Bischel <travis.bischel+github@gmail.com>
2026-02-02 02:24:38 -0700

    kfake main: support -as-version

--- pkg/kfake/main.go
+++ pkg/kfake/main.go
@@ -10,13 +10,16 @@ import (
 	"strings"
 
 	"github.com/twmb/franz-go/pkg/kfake"
+	"github.com/twmb/franz-go/pkg/kversion"
 )
 
 func main() {
 	// Configure log level from flag
 	var logLevelStr string
+	var versionStr string
 	flag.StringVar(&logLevelStr, "log-level", "none", "Log level: none, error, warn, info, debug")
 	flag.StringVar(&logLevelStr, "l", "none", "Log level (shorthand)")
+	flag.StringVar(&versionStr, "as-version", "", "Kafka version to emulate (e.g., 2.8, 3.5)")
 	flag.Parse()
 
 	logLevel := kfake.LogLevelNone
@@ -33,11 +36,20 @@ func main() {
 		logLevel = kfake.LogLevelNone
 	}
 
-	c, err := kfake.NewCluster(
+	opts := []kfake.Opt{
 		kfake.Ports(9092, 9093, 9094),
 		kfake.SeedTopics(-1, "foo"),
 		kfake.WithLogger(kfake.BasicLogger(os.Stderr, logLevel)),
-	)
+	}
+	if versionStr != "" {
+		v := kversion.FromString(versionStr)
+		if v == nil {
+			fmt.Fprintf(os.Stderr, "unknown version %q; valid versions: %v\n", versionStr, kversion.VersionStrings())
+			os.Exit(1)
+		}
+		opts = append(opts, kfake.MaxVersions(v))
+	}
+	c, err := kfake.NewCluster(opts...)
 	if err != nil {
 		panic(err)
 	}
