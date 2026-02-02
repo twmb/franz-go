twmb/frang

kfake: add support for transactions

pkg/kfake/00_produce.go
@@ -56,10 +56,6 @@ func (c *Cluster) handleProduce(b *broker, kreq kmsg.Request) (kmsg.Response, er
 		return resp
 	}
 
-	if req.TransactionID != nil {
-		donets(kerr.TransactionalIDAuthorizationFailed.Code)
-		return toresp(), nil
-	}
 	switch req.Acks {
 	case -1, 0, 1:
 	default:
@@ -116,26 +112,30 @@ func (c *Cluster) handleProduce(b *broker, kreq kmsg.Request) (kmsg.Response, er
 				b.MaxTimestamp = now
 				logAppendTime = now
 			}
-			if attrs&0xfff0 != 0 { // TODO txn bit
-				donep(rt.Topic, rp, kerr.CorruptMessage.Code)
-				continue
-			}
 			if b.LastOffsetDelta != b.NumRecords-1 {
 				donep(rt.Topic, rp, kerr.CorruptMessage.Code)
 				continue
 			}
 
-			seqs, epoch := c.pids.get(b.ProducerID, b.ProducerEpoch, rt.Topic, rp.Partition)
-			if be := b.ProducerEpoch; be != -1 {
-				if be < epoch {
-					donep(rt.Topic, rp, kerr.FencedLeaderEpoch.Code)
-					continue
-				} else if be > epoch {
-					donep(rt.Topic, rp, kerr.UnknownLeaderEpoch.Code)
-					continue
-				}
+			txnal := attrs&0xfff0 != 0
+			pid, seq := c.pids.get(b.ProducerID, b.ProducerEpoch, rt.Topic, rp.Partition)
+			if txnal && seq == nil {
+				donep(rt.Topic, rp, kerr.InvalidTxnState.Code)
+				continue
 			}
-			ok, dup := seqs.pushAndValidate(b.FirstSequence, b.NumRecords)
+			be := b.ProducerEpoch
+			switch {
+			case seq == nil && be != -1:
+				donep(rt.Topic, rp, kerr.InvalidTxnState.Code)
+				continue
+			case seq != nil && be < pid.epoch:
+				donep(rt.Topic, rp, kerr.FencedLeaderEpoch.Code)
+				continue
+			case seq != nil && be > pid.epoch:
+				donep(rt.Topic, rp, kerr.UnknownLeaderEpoch.Code)
+				continue
+			}
+			ok, dup := seq.pushAndValidate(b.FirstSequence, b.NumRecords)
 			if !ok {
 				donep(rt.Topic, rp, kerr.OutOfOrderSequenceNumber.Code)
 				continue
@@ -146,7 +146,10 @@ func (c *Cluster) handleProduce(b *broker, kreq kmsg.Request) (kmsg.Response, er
 			}
 			baseOffset := pd.highWatermark
 			lso := pd.logStartOffset
-			pd.pushBatch(len(rp.Records), b)
+			batchptr := pd.pushBatch(len(rp.Records), b, txnal)
+			if txnal {
+				pid.batches = append(pid.batches, batchptr)
+			}
 			sp := donep(rt.Topic, rp, 0)
 			sp.BaseOffset = baseOffset
 			sp.LogAppendTime = logAppendTime

twmb/franz-go

pkg/kfake/00_produce.go
@@ -77,10 +77,6 @@ func (c *Cluster) handleProduce(b *broker, kreq kmsg.Request) (kmsg.Response, er
 		return resp
 	}
 
-	if req.TransactionID != nil {
-		donets(kerr.TransactionalIDAuthorizationFailed.Code)
-		return toresp(), nil
-	}
 	switch req.Acks {
 	case -1, 0, 1:
 	default:
@@ -148,26 +144,30 @@ func (c *Cluster) handleProduce(b *broker, kreq kmsg.Request) (kmsg.Response, er
 				b.MaxTimestamp = now
 				logAppendTime = now
 			}
-			if attrs&0xfff0 != 0 { // TODO txn bit
-				donep(rt, rp, kerr.CorruptMessage.Code)
-				continue
-			}
 			if b.LastOffsetDelta != b.NumRecords-1 {
 				donep(rt, rp, kerr.CorruptMessage.Code)
 				continue
 			}
 
-			seqs, epoch := c.pids.get(b.ProducerID, b.ProducerEpoch, rt.Topic, rp.Partition)
-			if be := b.ProducerEpoch; be != -1 {
-				if be < epoch {
-					donep(rt, rp, kerr.FencedLeaderEpoch.Code)
-					continue
-				} else if be > epoch {
-					donep(rt, rp, kerr.UnknownLeaderEpoch.Code)
-					continue
-				}
+			txnal := attrs&0xfff0 != 0
+			seqs, window := c.pids.get(b.ProducerID, rt.Topic, rp.Partition)
+			if txnal && window == nil {
+				donep(rt, rp, kerr.InvalidTxnState.Code)
+				continue
 			}
-			ok, dup := seqs.pushAndValidate(b.FirstSequence, b.NumRecords)
+			be := b.ProducerEpoch
+			switch {
+			case window == nil && be != -1:
+				donep(rt, rp, kerr.InvalidTxnState.Code)
+				continue
+			case window != nil && be < seqs.epoch:
+				donep(rt, rp, kerr.FencedLeaderEpoch.Code)
+				continue
+			case window != nil && be > seqs.epoch:
+				donep(rt, rp, kerr.UnknownLeaderEpoch.Code)
+				continue
+			}
+			ok, dup := window.pushAndValidate(b.FirstSequence, b.NumRecords)
 			if !ok {
 				donep(rt, rp, kerr.OutOfOrderSequenceNumber.Code)
 				continue
@@ -178,7 +178,10 @@ func (c *Cluster) handleProduce(b *broker, kreq kmsg.Request) (kmsg.Response, er
 			}
 			baseOffset := pd.highWatermark
 			lso := pd.logStartOffset
-			pd.pushBatch(len(rp.Records), b)
+			batchptr := pd.pushBatch(len(rp.Records), b, txnal)
+			if txnal {
+				seqs.batches = append(seqs.batches, batchptr)
+			}
 			sp := donep(rt, rp, 0)
 			sp.BaseOffset = baseOffset
 			sp.LogAppendTime = logAppendTime